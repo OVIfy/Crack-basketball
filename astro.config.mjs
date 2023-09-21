cc26bf08571ad4be6a0632fa31aa62b928cd6eec
DESKTOP-13RQSSJ\HP <Ezechukwuchirimmadu@gmail.com>
2023-09-21 14:30:36 +0100

    feat: can contact us

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind(), react()]
+  integrations: [svelte(), tailwind(), react()],
+  output : "server",
+  adapter: netlify()
 });
